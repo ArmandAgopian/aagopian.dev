eb3bfe0d81fa0170163a22d2a51c04e609f45b4c
Armand Agopian <armandagopian457@gmail.com>
2025-10-02 21:33:12 -0700

    feat(tanstack): :art: Add cloudflare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   },
   plugins: [
     tsConfigPaths(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
     viteReact(),
